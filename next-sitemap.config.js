92d863491fbb9446504603a594e988a488cee5d3
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2025-03-01 15:30:25 +0100

    updated next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,17 @@
 const siteMetaData = require("./src/utils/siteMetaData");
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || siteMetaData.siteUrl, 
-  generateRobotsTxt: true, // (optional)
-  // ...other options
+  siteUrl: process.env.SITE_URL || siteMetaData.siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+  changefreq: 'weekly',
+  priority: 0.7,
+  exclude: ['/server-sitemap.xml'], // Utelat sider du ikke vil ha i sitemapet
 }
