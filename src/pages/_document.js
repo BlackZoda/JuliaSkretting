bc86f226bab75ebe3d4e013ef24aad0495ca5333
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2025-03-01 14:33:36 +0100

    added head section to _documents.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,17 @@
-<Head>
-  <link rel="icon" href="/JSSquare.svg" />
-  <link rel="icon" type="image/svg+xml" href="/JSSquare.svg" />
-  <link rel="apple-touch-icon" href="/JSSquare.svg" />
-</Head> 
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="icon" href="/JSSquare.svg" />
+        <link rel="icon" type="image/svg+xml" href="/JSSquare.svg" />
+        <link rel="apple-touch-icon" href="/JSSquare.svg" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
